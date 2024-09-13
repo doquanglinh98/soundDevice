c8bc5da197e21d1cfa472a95cfd90d2e3eb1d672
doquanglinh98 <quanglinh936@gmail.com>
2024-09-13 23:50:47 +0700

    create project

---
+++ src/main/java/com/vn/sound/controller/SoundController.java
@@ -0,0 +1,20 @@
+package com.vn.sound.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SoundController {
+
+	@GetMapping("/info")
+	ResponseEntity<String> inf() {
+		return new ResponseEntity<>("App is running!", HttpStatus.OK);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "views/home";
+	}
+}
