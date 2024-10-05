f415399a5bdc1aa4bff8b8d54d4ce4b4951b8a48
linhdq <linhdq62@wru.vn>
2024-10-05 13:07:23 +0700

    revert common

--- src/main/java/com/vn/sound/common/Constant.java
+++ src/main/java/com/vn/sound/common/Constant.java
@@ -2,22 +2,22 @@ package com.vn.sound.common;
 
 public class Constant {
 	public static final String uploadDir_N9Speakers = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/Speakers/";
+			+ "/src/main/resources/static/imgs/Speakers/";
 	public static final String uploadDir_Ampli = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/Amplifiers/";
+			+ "/src/main/resources/static/imgs/Amplifiers/";
 	public static final String uploadDir_micro = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/Micros/";
+			+ "/src/main/resources/static/imgs/Micros/";
 	public static final String uploadDir_Mixer = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/Mixers/";
+			+ "/src/main/resources/static/imgs/Mixers/";
 
 	public static final String uploadDir_N9Speakers_Series = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/SpeakersSeries/";
+			+ "/src/main/resources/static/imgs/SpeakersSeries/";
 	public static final String uploadDir_Ampli_Series = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/AmplifiersSeries/";
+			+ "/src/main/resources/static/imgs/AmplifiersSeries/";
 	public static final String uploadDir_micro_Series = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/MicrosSeries/";
+			+ "/src/main/resources/static/imgs/MicrosSeries/";
 	public static final String uploadDir_Mixer_Series = System.getProperty("user.dir")
-			+ "https://tscproaudio.com/imgs/MixersSeries/";
+			+ "/src/main/resources/static/imgs/MixersSeries/";
 
 	public static final String host = "https://tscproaudio.com/";
 
