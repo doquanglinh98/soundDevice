4866f9d65933954b13d3f399fe97e2ee72a46dd3
trungdt <trungdoan7373@gmail.com>
2024-10-05 22:38:44 +0700

    bootstrap error

--- src/main/resources/static/js/api/admin/micro.js
+++ src/main/resources/static/js/api/admin/micro.js
@@ -1,6 +1,4 @@
 const API_URL = "https://tscproaudio.com/manager";
-const successToast = new bootstrap.Toast(".toast-success");
-const failToast = new bootstrap.Toast(".toast-fail");
 
 const basicAuth = localStorage.getItem("basicAuth");
 
@@ -222,6 +220,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+  const successToast = new bootstrap.Toast(".toast-success");
+  const failToast = new bootstrap.Toast(".toast-fail");
+
   const form = document.getElementById("create-micro-form");
   if (form) {
     form.addEventListener("submit", async function (event) {
